c31f5c9660bc24a6554284fd992e6a4dc299098c
Flex Coder <jaychonkar617@gmail.com>
2025-06-07 23:51:16 +0530

    index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/', async (req, res) => {
       await sendMessage(chat_id, `👋 <b>Welcome to the bot!</b>`, message_id, {
         inline_keyboard: [
           [{ text: '➕ Add to Group', url: `https://t.me/${botUsername}?startgroup=true` }],
-          [{ text: '📢 Join Channel', url: 'https://t.me/YOUR_CHANNEL_USERNAME' }]
+          [{ text: '📢 Join Channel', url: https://t.me/${botUsername}' }]
         ]
       })
     }
